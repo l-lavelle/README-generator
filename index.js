770d1c29326f086ad7ceae5189e5b1db338fef6f
Lauren <lauren.m.lavelle@wmich.edu>
2023-07-27 18:52:23 -0400

    installed inquirer

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:"input",
+        message:"What is the title of the project?",
+        name:"title"
+    },
+    {
+        type: "input",
+        message: "Give a brief description of your project",
+        name: "description"
+    },
+    {
+        type:"input",
+        message:"What are the steps required to install your project?",
+        name:"installation"
+    },
+    {
+        type:"input",
+        message:"Provide insturctions for how to use application",
+        name:"usage"
+    },
+    {
+        type:"list",
+        message:"Choose which license you are using",
+        choices:["Apache License 2.0","GNU General Public License v3.0","MIT License"],
+        name: "license"
+    }
+// Add License, Contributing, Tests, and Questions, github username, email address
+// https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
