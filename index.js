581feeb8f7adfb016c74eac9b72e16d28530cdd4
Lauren <lauren.m.lavelle@wmich.edu>
2023-07-27 21:07:23 -0400

    able to read and write input data

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // TODO: Include packages needed for this application
+const fs = require('fs')
+const generateMd= require('./utils/generateMarkdown')
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [
+const projectQuestions = [
     {
         type:"input",
         message:"What is the title of the project?",
@@ -29,15 +31,43 @@ const questions = [
         choices:["Apache License 2.0","GNU General Public License v3.0","MIT License"],
         name: "license"
     }
+
+
+
+
 // Add License, Contributing, Tests, and Questions, github username, email address
 // https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    // works
+    const {title, description,installation,usage,license}=data
+    console.log(license)
+
+
+    
+
+    // works
+    // const trial = JSON.parse(JSON.stringify(data))
+    // console.log(trial.title)
+
+
+    const templateliteral=license
+    fs.writeFile(fileName,templateliteral, (err)=>
+    {if (err){
+        console.error(err)
+    }}
+    )
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(projectQuestions)
+    .then((response)=> 
+        {writeToFile("README.md",response)});
+}
 
 // Function call to initialize app
 init();
