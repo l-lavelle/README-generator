ca8d7747d6c93ae5af33d27ee0854129d254648b
Lauren <lauren.m.lavelle@wmich.edu>
2023-08-01 20:10:47 -0400

    All input validated

--- index.js
+++ index.js
@@ -8,50 +8,117 @@ const projectQuestions = [
     {
         type:"input",
         message:"What is the title of the project?",
-        name:"title"
+        name:"title",
+        validate: function(title){
+            if (title.trim()===""){
+                return console.log("\nPlease enter a title")
+            } else {
+                return true
+            }
+        }
     },
     {
         type: "input",
-        message: "Give a brief description of your project",
-        name: "description"
+        message: "Give a brief description of your project. Include what your application does and technologies used.",
+        name: "description",
+        validate: function(description){
+            if (description.trim()===""){
+                return console.log("\nPlease enter a description")
+            } else {
+                return true
+            }
+        }
     },
     {
         type:"input",
-        message:"What are the steps required to install your project?",
-        name:"installation"
+        message:"What are the steps required to install your project? Are there any required dependencies?",
+        name:"installation",
+        validate: function(installation){
+            if (installation.trim()===""){
+                return console.log("\nPlease explain if there is any installation needed. If none enter N/A.")
+            } else {
+                return true
+            }
+        }
     },
     {
         type:"input",
-        message:"Provide insturctions for how to use application",
-        name:"usage"
+        message:"Provide instructions for how to use application:",
+        name:"usage",
+        validate: function(usage){
+            if (usage.trim()===""){
+                return console.log("\nPlease provide instructions for how to use application.")
+            } else {
+                return true
+            }
+        }
     },
     {
         type:"list",
-        message:"Choose which license you are using",
-        choices:["None","Apache 2.0","MIT"],
+        message:"Choose which license you are using?",
+        choices:["None","Apache 2.0","MIT","GPLv2","GPLv3","Unlicense"],
         name: "license"
     },
     {
-        type:"input",
-        message:"Name any contributers",
-        name:"contributers"
+        type:"input", 
+        message:"Name any contributers that helped you collaborate on the project.",
+        name:"contributers",
+        validate: function(contributers){
+            if (contributers.trim()===""){
+                return console.log("\nPlease provide contributors. If none enter N/A.")
+            } else {
+                return true
+            }
+        }
+    },
+    {
+        type:"input", 
+        message:"If you used any websites to help on project enter the website? (Enter a comma between each site)",
+        name:"websites",
+        validate: function(websites){
+            if (/^https?:\/\/\S+/.test(websites)){
+                return true
+            } else{
+                return console.log("\nPlease enter website in format http(s)://")
+            }
+        }
     },
     {
         type:"input",
-        message:"Are there any tests",
-        name:"test"
+        message:"If you wrote any tests for your application describe them",
+        name:"test",
+        validate: function(test){
+            if (test.trim()===""){
+                return console.log("\nEnter test description for project. If none enter N/A.")
+            } else {
+                return true
+            }
+        }
     },
     {
         type:"input",
-        message:"Please enter github username",
-        name:"github"
+        message:"Please enter your github username",
+        name:"github",
+        validate: function(github){
+            if (github.trim()===""){
+                return console.log("\nPlease enter your Github username.")
+            } else {
+                return true
+            }
+        }
     },
     {
         type:"input",
         message:"Please enter email address",
-        name:"email"
+        name:"email",
+        validate: function(email){
+            if (/\S+@\S+\.\S+/.test(email)){
+                return true
+            } else{
+                return console.log("\nPlease enter email in format bob@email.com")
+            }
+        }
     }
-// https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
 ];
 
 // TODO: Create a function to write README file
@@ -67,7 +134,7 @@ function writeToFile(fileName, data) {
 function init() {
     inquirer.prompt(projectQuestions)
     .then((response)=> 
-        {writeToFile("README.md",generateMd(response))},
+        {writeToFile("Test.md",generateMd(response))},
       )
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,27 +1,35 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license==="Apache 2.0"){
-  const licensebadge="![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)"
+  if (license!="None"){
+  let editedLicense = license.replace(" ","_")
+  const licensebadge=`![License](https://img.shields.io/badge/License-${editedLicense}-blue.svg)`
   return licensebadge
-}else if (license==="MIT License"){
-  const licensebadge="![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)"
-  return licensebadge
-}else if (license==="None"){
+} else {
   const licensebadge=""
   return licensebadge
 }}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
+
 function renderLicenseLink(license) {
   if (license==="Apache 2.0"){
-    const licenseLink="[Apache 2.0](https://opensource.org/licenses/ISC)"
+    const licenseLink="[Apache 2.0](https://choosealicense.com/licenses/apache-2.0/)"
     return licenseLink
   }else if (license==="MIT"){
-    const licenseLink="[MIT](https://opensource.org/licenses/MIT)"
+    const licenseLink="[MIT](https://choosealicense.com/licenses/mit/)"
     return licenseLink
-  }else if (license==="None"){
+  }else if (license==="GPLv2"){
+    const licenseLink="[General Public License v2.0](https://choosealicense.com/licenses/gpl-2.0/)"
+    return licenseLink
+  }else if (license==="GPLv3"){
+    const licenseLink="[General Public License v3.0](https://choosealicense.com/licenses/gpl-3.0/)"
+    return licenseLink
+  }else if (license==="Unlicense"){
+    const licenseLink="[Unlicense](https://choosealicense.com/licenses/unlicense/)"
+    return licenseLink
+  } else if (license==="None"){
     const licenseLink=""
     return licenseLink
   }
@@ -30,14 +38,29 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
+  if (license !="None"){
+    return `The license for this project is ${license}.<br />
+    Please click the link below to view.`
+  } else {
+    return `No license used for this project.`
+  }
 
+}
+
+function webAddresses(websites){
+  const websiteArray = websites.split(",");
+  var allSites = ""
+  for (const website of websiteArray) {  
+    allSites += `${website} <br />`
+  }
+  return allSites
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  const {title,description,installation,usage, license, contributers, test, github, email}=data
+  const {title,description,installation,usage, license, contributers, websites, test, github, email}=data
   
-  return `# ${title}
+  return `# ${title.trim()}
   ${renderLicenseBadge(license)}
   ## Description
   ${description}
@@ -57,16 +80,19 @@ ${installation}
 ${usage}
 
 ## License 
+${renderLicenseSection(license)}<br />
 ${renderLicenseLink(license)}
 
 ## Contributing
-${contributers}
+${contributers}<br />
+Websites used for refernce:<br />
+${webAddresses(websites)}
 
 ## Tests
 ${test}
 
 ## Questions
-
+Feel free to reach out to me with any questions via github or email!<br />
 [Github Profile](https://github.com/${github})
 
 Email: ${email}
@@ -76,3 +102,4 @@ Email: ${email}
 module.exports = generateMarkdown;
 
 // license website: https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
+// https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
